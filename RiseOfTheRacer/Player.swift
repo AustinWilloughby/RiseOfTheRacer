83569921d5fcdd46cac7e936c30b9c956c1a4e9d
Ryan <rck1464@rit.edu>
2015-11-18 11:51:15 -0500

    Added Player Class

---
+++ RiseOfTheRacer/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  RiseOfTheRacer
+//
+//  Created by igmstudent on 11/18/15.
+//  Copyright © 2015 BigTipperGames. All rights reserved.
+//
+
+import SpriteKit
+
+class Player: SKSpriteNode {
+    var pos:CGPoint = CGPoint(x: 0, y: 0)
+    var vel:CGPoint = CGPoint(x: 0, y: 0)
+    var acl:CGPoint = CGPoint(x: 0, y: 0)
+    
+    
+}
