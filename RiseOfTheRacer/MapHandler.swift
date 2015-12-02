ff24d26626bba6b668442d8da86554e18d188c13
Austin Willoughby <arw8020@rit.edu>
2015-12-02 11:49:55 -0500

    Added StreamReader, Began MapHandler
    Added a stream reader class for reading in data from files.  This was
    taken from a StackOverflow post that is linked in the comments.  Also
    began working on the map handler.

--- RiseOfTheRacer/MapHandler.swift
+++ RiseOfTheRacer/MapHandler.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import SpriteKit
+
+class MapHandler{
+    private var level:Int = 1
+    internal var tileArray = [[Tile]]()
+    
+    init(){
+        level = 1
+    }
+    
+    
+    func ReadMap(mapFilePath: String){
+        let input = StreamReader(path: mapFilePath)
+        
+        var currentLine = input?.nextLine()
+        var coords = currentLine?.characters.split{$0 == ","}.map(String.init)
+        var xSize:Int = Int(coords![0])!
+        var ySize:Int = Int(coords![1])!
+    }
+    
+    
+}
